d0f0344de82296b8c18235a437f220debdad2e31
Marcin Gizka <marcin.gizka@.com>
2021-11-23 18:47:54 +0100

    server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const fs = require('fs');
+const app = express()
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/invoices/:id', (req, res) => {
+    let rawdata = fs.readFileSync('./invoices/'+req.params.id+'.json');
+
+    res.setHeader('content-type', 'application/json');
+    res.send(rawdata)
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening at ${port}`)
+})
